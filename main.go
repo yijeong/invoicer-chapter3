d7024001eaf9147fef8b324cd9954a47c849a749
Cho <yijeong@f01898a3fa41.ant.amazon.com>
2021-05-26 15:31:31 +0900

    add content type options header

--- main.go
+++ main.go
@@ -209,6 +209,7 @@ func (iv *invoicer) deleteInvoice(w http.ResponseWriter, r *http.Request) {
 func (iv *invoicer) getIndex(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Security-Policy", "default-src 'self'; child-src 'self;")
 	w.Header().Add("X-Frame-Options", "SAMEORIGIN")
+	w.Header().Add("X-Content-Type-Options", "nosniff")
 	w.Write([]byte(`
 <!DOCTYPE html>
 <html>
